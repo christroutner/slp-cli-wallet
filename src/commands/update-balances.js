5bf2786ff778d8649b986d692c8882ad194b715f
Chris Troutner <chris.troutner@gmail.com>
2020-11-01 21:17:21 -0800

    fix(update-balances): Better handling of empty wallets

--- src/commands/update-balances.js
+++ src/commands/update-balances.js
@@ -107,13 +107,20 @@ class UpdateBalances extends Command {
       walletInfo,
       flags.ignoreTokens
     )
-    // console.log(`addressData: ${JSON.stringify(addressData, null, 2)}`)
+    console.log(`addressData: ${JSON.stringify(addressData, null, 2)}`)
 
     // Exit if this is a new wallet that does not need to be updated.
     if (addressData.addressData.length === 0) {
-      return {
-        balance: 0
-      }
+      walletInfo.balance = 0
+      walletInfo.balanceConfirmed = 0
+      walletInfo.balanceUnconfirmed = 0
+      walletInfo.hasBalance = []
+      walletInfo.SLPUtxos = []
+      walletInfo.BCHUtxos = []
+
+      await this.appUtils.saveWallet(filename, walletInfo)
+
+      return walletInfo
     }
 
     // Update hasBalance array with non-zero balances.
